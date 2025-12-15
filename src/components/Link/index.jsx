7622599532445dd8550bfce3b7d8aeb72a8b48ad
Igor Josiel <igorjosielvitalino@gmail.com>
2025-12-15 17:21:20 -0300

    agrupa rotas relacionadas com nested routes, implementa Linking e implementa rotas protegidas.

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -1,10 +1,12 @@
-import styles from './link.module.css'
+import { Link as RouterLink } from "react-router";
+import styles from "./link.module.css";
 
-export const Link = ({ children, ...props }) => {
-    const className = props.className || ''
-    return (
-        <a {...props} className={`${styles.link} ${className}`}>
-            {children}
-        </a>
-    )
-}
+export const Link = ({ children, href, ...props }) => {
+  const className = props.className || "";
+
+  return (
+    <RouterLink to={href} className={`${styles.link} ${className}`} {...props}>
+      {children}
+    </RouterLink>
+  );
+};
