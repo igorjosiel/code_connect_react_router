d46f745f88fd42ae4ca6b1b9ec9cb9f498107677
Igor Josiel <igorjosielvitalino@gmail.com>
2025-12-17 17:19:35 -0300

    fix: ajusta arquivo do componente Providers.

--- src/components/Providers/index.jsx
+++ src/components/Providers/index.jsx
@@ -1,22 +1,21 @@
-import { IconButton } from "../IconButton"
-import google from './google.png'
-import github from './github.png'
-
-import styles from './providers.module.css'
+import { IconButton } from "../IconButton";
+import google from "./google.png";
+import github from "./github.png";
+import styles from "./providers.module.css";
 
 export const Providers = () => {
-    return (
-        <ul className={styles.providers}>
-            <li>
-                <IconButton>
-                    <img src={google} alt="Google" />
-                </IconButton>
-            </li>
-            <li>
-                <IconButton>
-                    <img src={github} alt="Google" />
-                </IconButton>
-            </li>
-        </ul>
-    )
-}
+  return (
+    <ul className={styles.providers}>
+      <li>
+        <IconButton>
+          <img src={google} alt="Google" />
+        </IconButton>
+      </li>
+      <li>
+        <IconButton>
+          <img src={github} alt="Github" />
+        </IconButton>
+      </li>
+    </ul>
+  );
+};
